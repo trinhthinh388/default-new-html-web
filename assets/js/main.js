31df39dc016ac91e2c7bc29d62d0f9441a302acd
Trinh Duc Thinh <trinhthinh388@gmail.com>
2020-06-04 09:19:51 +0700

    add teams section

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,7 +115,37 @@ $(document).ready(function(){
         slidesToScroll: 1,
     });
 
-    $('.slider__inner').on('init', function(slick){
+    $('.slider__row--team').slick({
+        dots: true,
+        loop: true,
+        infinite: true,
+        prevArrow: null,
+        nextArrow: null,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings:{
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings:{
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
     //END
 
